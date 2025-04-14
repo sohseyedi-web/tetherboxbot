0e9c36597b0f1207cb2a1141377ca7932cc245ff
sohseyedi <sohseyedi@gmail.com>
2025-04-14 13:22:07 +0330

    chore:add price and help commands to bot

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const { Telegraf } = require("telegraf");
 require("dotenv").config();
 
-const { onStart } = require("./app/handlers/botHandlers");
+const { onStart, onPrice, onHelp } = require("./app/handlers/botHandlers");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start(onStart);
+bot.hears("قیمت تتر", onPrice);
+bot.command("help", onHelp);
+bot.command("price", onPrice);
+bot.hears("درباره ربات", onHelp);
 
 // Start bot
 bot.launch();
