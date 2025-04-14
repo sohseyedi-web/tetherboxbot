a1cd81962b1785c34604ff2756fa9a5b322712fd
sohseyedi <sohseyedi@gmail.com>
2025-04-14 13:50:21 +0330

    chore:add subscription and interval handling commands to bot

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const { Telegraf } = require("telegraf");
 require("dotenv").config();
 
-const { onStart, onPrice, onHelp } = require("./app/handlers/botHandlers");
+const {
+  onStart,
+  onPrice,
+  onHelp,
+  onSubscribe,
+  onSetInterval,
+  onReturn,
+  setupNotificationSystem,
+} = require("./app/handlers/botHandlers");
+
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// Main commands
 bot.start(onStart);
 bot.hears("قیمت تتر", onPrice);
 bot.command("help", onHelp);
 bot.command("price", onPrice);
 bot.hears("درباره ربات", onHelp);
 
+// Subscription commands
+bot.hears("دریافت خودکار قیمت", onSubscribe);
+bot.hears("بازگشت", onReturn);
+
+// Handle interval input (any number or predefined buttons)
+bot.hears(/^[0-9]+$/, onSetInterval);
+
+// Setup the notification system
+setupNotificationSystem(bot);
+
 // Start bot
 bot.launch();
 console.log("🤖 Bot is running...");
 
+// Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
