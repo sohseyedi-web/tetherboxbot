77f1c39f251f164b9eb0ff5830ca36e9b78a492a
sohseyedi <sohseyedi@gmail.com>
2025-04-14 14:58:57 +0330

    chore: add utility functions to convert digits and format price messages

--- app/utils/functions.js
+++ app/utils/functions.js
@@ -35,8 +35,43 @@ async function fetchPrice(url, name, extractPrice) {
   }
 }
 
+function convertToEnglishDigits(str) {
+  const persianDigits = /[۰-۹]/g;
+  return str.replace(persianDigits, function (d) {
+    return String.fromCharCode(d.charCodeAt(0) - 1728);
+  });
+}
+
+// Utility function to sort prices from highest to lowest
+function sortPriceResults(results) {
+  return [...results].sort((a, b) => {
+    const priceStrA = convertToEnglishDigits(a.price);
+    const priceStrB = convertToEnglishDigits(b.price);
+
+    const priceA = parseInt(priceStrA.replace(/[^\d]/g, ""));
+    const priceB = parseInt(priceStrB.replace(/[^\d]/g, ""));
+
+    return priceB - priceA;
+  });
+}
+
+function formatPriceMessage(results) {
+  let message = "🔰 قیمت تتر:\n\n";
+
+  // Sort the results from highest to lowest
+  const sortedResults = sortPriceResults(results);
+
+  sortedResults.forEach((result) => {
+    message += `${result.name}: ${result.price}\n\n`;
+  });
+
+  return message;
+}
+
 module.exports = {
   toPersianNumbersWithComma,
   toPersianNumbers,
   fetchPrice,
+  sortPriceResults,
+  formatPriceMessage,
 };
