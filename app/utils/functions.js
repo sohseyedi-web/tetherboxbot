18b0fe03b9ca81b80c8d582d7bcac806f11c2e57
sohseyedi <sohseyedi@gmail.com>
2025-04-14 13:21:13 +0330

    chore:add fetchPrice function to retrieve and format prices from a given URL

--- app/utils/functions.js
+++ app/utils/functions.js
@@ -1,3 +1,4 @@
+const axios = require("axios");
 const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
 
 function toPersianNumbersWithComma(n) {
@@ -14,7 +15,28 @@ function toPersianNumbers(n) {
   return n.toString().replace(/\d/g, (x) => farsiDigits[parseInt(x)]);
 }
 
+async function fetchPrice(url, name, extractPrice) {
+  try {
+    const { data } = await axios.get(url);
+    let price = extractPrice(data);
+
+    return {
+      name,
+      price: price
+        ? `${toPersianNumbersWithComma(price)} تومان`
+        : "قیمت پیدا نشد",
+    };
+  } catch (error) {
+    console.error(`خطا در دریافت قیمت از ${name}:`, error.message);
+    return {
+      name,
+      price: "خطا در دریافت",
+    };
+  }
+}
+
 module.exports = {
   toPersianNumbersWithComma,
   toPersianNumbers,
+  fetchPrice,
 };
